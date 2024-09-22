9109cd11dce6caea8b350abf521787f3d608ddc3
José Ángel López <jose.a.l.e@hotmail.com>
2024-09-22 21:32:47 +0200

    feature: add simple js

---
+++ frontend/js/app.js
@@ -0,0 +1,6 @@
+const sortButton = document.getElementById('sortButton');
+const sortInput = document.getElementById('sortInput')
+
+sortButton.addEventListener('click', () => {
+    console.log(sortInput.value);
+});
