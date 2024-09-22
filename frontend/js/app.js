e522d6512066904d4f6ca2b4199213cc7fb58fe5
José Ángel López <jose.a.l.e@hotmail.com>
2024-09-22 21:45:41 +0200

    feature: initial algorithms condition WIP

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -3,6 +3,26 @@ const sortInput = document.getElementById('sortInput')
 const sortAlgorithm = document.getElementById('sortOptions')
 
 sortButton.addEventListener('click', () => {
-    console.log(sortInput.value);
-    console.log(sortAlgorithm.value);
+    sortAlgorithm = sortAlgorithm.value;
+    const inputValues = sortInput.value.split(',').map(Number);
+
+    if (selectedAlgorithm === "Bubble Sort") {
+        console.log('Perform Bubble Sort on', inputValues);
+    }
+
+    else if (selectedAlgorithm === "Selection Sort") {
+        console.log('Perform Selection Sort on', inputValues);
+    }
+
+    else if (selectedAlgorithm === "Insertion Sort") {
+        console.log('Perform Insertion Sort on', inputValues);
+    }
+
+    else if (selectedAlgorithm === "Merge Sort") {
+        console.log('Perform Merge Sort on', inputValues);
+    }
+
+    else if (selectedAlgorithm === "Quick Sort") {
+        console.log('Perform Quick Sort on', inputValues);
+    }
 });
