ac14558707346e04f081a8457a3b53e646686b01
José Ángel López <jose.a.l.e@hotmail.com>
2024-09-22 21:40:32 +0200

    fix: value instead of id

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -1,6 +1,8 @@
 const sortButton = document.getElementById('sortButton');
 const sortInput = document.getElementById('sortInput')
+const sortAlgorithm = document.getElementById('sortOptions')
 
 sortButton.addEventListener('click', () => {
     console.log(sortInput.value);
+    console.log(sortAlgorithm.value);
 });
